754c61cd8d3c67cbeb0f6f503827ab2cca1b43f0
Vlad Ivanov <himametsu@gmail.com>
2022-12-24 12:13:47 +0800

    add global layout and styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="layout">
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
-#app {
+body {
+  height: 100vh;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+  #app {
+    height: 100%;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    .layout {
+      height: 100%;
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 16px 0;
 
-    &.router-link-exact-active {
-      color: #42b983;
+      @media screen and (max-width: 1200px) {
+        padding: 16px;
+      }
     }
+  }
+
+  * {
+    box-sizing: border-box;
   }
 }
 </style>
