90d328bff8f556d40c6d65c8acdde9b53a0e6ff6
Vlad Ivanov <himametsu@gmail.com>
2022-12-24 12:07:36 +0800

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    notes: [],
   },
   getters: {
+    getNoteById: (state) => (id) => {
+      return state.notes.find((note) => note.id == id);
+    },
   },
   mutations: {
+    addNote(state, note) {
+      state.notes = [...state.notes, note];
+    },
+    deleteNote(state, noteId) {
+      const index = state.notes.findIndex((note) => note.id == noteId);
+
+      if (index >= 0) {
+        state.notes = [...state.notes.slice(0, index), ...state.notes.slice(index + 1)];
+      }
+    },
+    loadNotes(state) {
+      const localNotes = localStorage.getItem("notes");
+
+      if (localNotes) {
+        state.notes = JSON.parse(localNotes);
+      }
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
